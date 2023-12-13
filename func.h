14151b011b32ffb4bb8ef2590c2310a2ea34cfca
manyakhinaem <lolkek@gmail.com>
2023-12-13 18:12:47 +0300

    start

---
+++ func.h
@@ -0,0 +1,14 @@
+#ifndef LAB2_H
+#define LAB2_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//void opp(int opa);
+
+void er();
+
+void plus(int a, int b, int opa, int opb);
+
+#endif
+
